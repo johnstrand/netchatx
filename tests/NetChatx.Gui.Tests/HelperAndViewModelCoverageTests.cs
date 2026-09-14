7f8d5c9a9ec397ca28800c6db881bca34735dd2b
John Strand <john.strand@dekiru.se>
2026-09-14 13:52:14 +0200

    Fixed failing tests

--- tests/NetChatx.Gui.Tests/HelperAndViewModelCoverageTests.cs
+++ tests/NetChatx.Gui.Tests/HelperAndViewModelCoverageTests.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Documents;
 using Avalonia.Media;
 using Avalonia.Input;
+using Avalonia.Headless.XUnit;
 using Avalonia.Layout;
 using NetChatx.Gui.Helpers;
 using NetChatx.Gui.Helpers.Markdown;
@@ -367,59 +362,39 @@ public class HelperAndViewModelCoverageTests
         Assert.Contains("🎉", partySearch);
     }
 
-    private static void EnsureAppInitialized()
-    {
-        if (Application.Current is null)
-        {
-            Program.BuildAvaloniaApp()
-                .SetupWithoutStarting();
-        }
-    }
 
-    [Fact]
+    [AvaloniaFact]
     public void LoginView_ConnectButton_AlignmentIsCentered()
     {
-        EnsureAppInitialized();
-
-        Avalonia.Threading.Dispatcher.UIThread.Invoke(() =>
-        {
-            var loginView = new LoginView();
-            var connectBtn = loginView.FindControl<Button>("ConnectButton");
-            Assert.NotNull(connectBtn);
-            Assert.Equal(HorizontalAlignment.Center, connectBtn.HorizontalContentAlignment);
-            Assert.Equal(VerticalAlignment.Center, connectBtn.VerticalContentAlignment);
-            Assert.Equal(new Thickness(0), connectBtn.Padding);
-        });
+        var loginView = new LoginView();
+        var connectBtn = loginView.FindControl<Button>("ConnectButton");
+        Assert.NotNull(connectBtn);
+        Assert.Equal(HorizontalAlignment.Center, connectBtn.HorizontalContentAlignment);
+        Assert.Equal(VerticalAlignment.Center, connectBtn.VerticalContentAlignment);
+        Assert.Equal(new Thickness(0), connectBtn.Padding);
     }
 
-    [Fact]
+    [AvaloniaFact]
     public void LoginView_EnterInUsernameField_HandledAndFocusesPassword()
     {
-        EnsureAppInitialized();
+        var loginView = new LoginView();
+        Assert.NotNull(loginView.JidTextBoxControl);
+        Assert.NotNull(loginView.PasswordTextBoxControl);
 
-        Avalonia.Threading.Dispatcher.UIThread.Invoke(() =>
-        {
-            var loginView = new LoginView();
-            Assert.NotNull(loginView.JidTextBoxControl);
-            Assert.NotNull(loginView.PasswordTextBoxControl);
+        // Key that is not Enter should not be handled
+        var nonEnterArgs = new KeyEventArgs { Key = Key.Tab, RoutedEvent = InputElement.KeyDownEvent };
+        loginView.OnJidKeyDown(loginView.JidTextBoxControl, nonEnterArgs);
+        Assert.False(nonEnterArgs.Handled);
 
-            // Key that is not Enter should not be handled
-            var nonEnterArgs = new KeyEventArgs { Key = Key.Tab, RoutedEvent = InputElement.KeyDownEvent };
-            loginView.OnJidKeyDown(loginView.JidTextBoxControl, nonEnterArgs);
-            Assert.False(nonEnterArgs.Handled);
-
-            // Enter key should be handled
-            var enterArgs = new KeyEventArgs { Key = Key.Enter, RoutedEvent = InputElement.KeyDownEvent };
-            loginView.OnJidKeyDown(loginView.JidTextBoxControl, enterArgs);
-            Assert.True(enterArgs.Handled);
-        });
+        // Enter key should be handled
+        var enterArgs = new KeyEventArgs { Key = Key.Enter, RoutedEvent = InputElement.KeyDownEvent };
+        loginView.OnJidKeyDown(loginView.JidTextBoxControl, enterArgs);
+        Assert.True(enterArgs.Handled);
     }
 
-    [Fact]
+    [AvaloniaFact]
     public void LoginView_EnterInPasswordField_TriggersConnect()
     {
-        EnsureAppInitialized();
-
         bool loginInvoked = false;
         var vm = new LoginViewModel(profile =>
         {
@@ -431,22 +406,19 @@ public class HelperAndViewModelCoverageTests
             Password = "secretpassword"
         };
 
-        Avalonia.Threading.Dispatcher.UIThread.Invoke(() =>
-        {
-            var loginView = new LoginView { DataContext = vm };
-            Assert.NotNull(loginView.PasswordTextBoxControl);
+        var loginView = new LoginView { DataContext = vm };
+        Assert.NotNull(loginView.PasswordTextBoxControl);
 
-            // Non-enter key should not trigger connect
-            var nonEnterArgs = new KeyEventArgs { Key = Key.Space, RoutedEvent = InputElement.KeyDownEvent };
-            loginView.OnPasswordKeyDown(loginView.PasswordTextBoxControl, nonEnterArgs);
-            Assert.False(nonEnterArgs.Handled);
-            Assert.False(loginInvoked);
+        // Non-enter key should not trigger connect
+        var nonEnterArgs = new KeyEventArgs { Key = Key.Space, RoutedEvent = InputElement.KeyDownEvent };
+        loginView.OnPasswordKeyDown(loginView.PasswordTextBoxControl, nonEnterArgs);
+        Assert.False(nonEnterArgs.Handled);
+        Assert.False(loginInvoked);
 
-            // Enter key should trigger connect
-            var enterArgs = new KeyEventArgs { Key = Key.Enter, RoutedEvent = InputElement.KeyDownEvent };
-            loginView.OnPasswordKeyDown(loginView.PasswordTextBoxControl, enterArgs);
-            Assert.True(enterArgs.Handled);
-            Assert.True(loginInvoked);
-        });
+        // Enter key should trigger connect
+        var enterArgs = new KeyEventArgs { Key = Key.Enter, RoutedEvent = InputElement.KeyDownEvent };
+        loginView.OnPasswordKeyDown(loginView.PasswordTextBoxControl, enterArgs);
+        Assert.True(enterArgs.Handled);
+        Assert.True(loginInvoked);
     }
 }
