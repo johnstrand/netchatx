4217a1c9fc03ce43a5ebc6bd46a3eef5af6fae1e
John Strand <johnstrand981@gmail.com>
2026-09-25 09:34:20 +0200

    feat: allow custom accent colors and chat bubble text colors (#84) (#94)

--- src/Stanza.Gui/ViewModels/MainChatViewModel.cs
+++ src/Stanza.Gui/ViewModels/MainChatViewModel.cs
@@ -540,6 +540,10 @@ public sealed partial class MainChatViewModel : ViewModelBase
             onBubbleColorChanged: (outColor, inColor) =>
             {
                 DirectionToBackgroundConverter.SetColors(outColor, inColor);
+                if (Settings is not null)
+                {
+                    DirectionToForegroundConverter.SetColors(Settings.OutboundBubbleTextColor, Settings.InboundBubbleTextColor);
+                }
                 foreach (var conv in Conversations)
                 {
                     foreach (var msg in conv.Messages)
@@ -790,6 +794,7 @@ public sealed partial class MainChatViewModel : ViewModelBase
             MessageBubbleViewModel.ShowInlinePreviews = Settings.ShowInlinePreviews;
             MessageBubbleViewModel.AutoDownloadMedia = Settings.AutoDownloadMedia;
             DirectionToBackgroundConverter.SetColors(Settings.OutboundBubbleColor, Settings.InboundBubbleColor);
+            DirectionToForegroundConverter.SetColors(Settings.OutboundBubbleTextColor, Settings.InboundBubbleTextColor);
             foreach (var conv in Conversations)
             {
                 foreach (var msg in conv.Messages)
